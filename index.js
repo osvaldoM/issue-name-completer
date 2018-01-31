d639e4fef75a4b10876e62130b349c975dda196d
osvaldom <mekymaria@gmail.com>
2018-01-31 13:26:12 +0200

    format issue

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const gitHubApi = require('github'),
       prettyjson = require('prettyjson'),
-      dotEnv = require('dotenv').config();
+      ncp = require("copy-paste"),
+      dotEnv = require('dotenv').config(),
+      owner_name = process.env.GH_USER,
+      repo_name = process.env.REPO_NAME;
 
-
-let github = new gitHubApi({
-});
+let github = new gitHubApi({});
 
 github.authenticate({
     type: 'oauth',
@@ -12,17 +13,24 @@ github.authenticate({
   });
   
 github.issues.get({
-  owner: process.env.GH_USER,
-  repo:process.env.REPO_NAME,
+  owner: owner_name,
+  repo: repo_name,
   number:'1007'
 }, function (err, res) {
   if (err){
     throw err;
   } 
   let content = res.data.body.split('\n');
-  content.forEach(line => {
-      console.log(line);
-    let       
+  let parsedContent= content.filter(val => {return val!="\r"});
+  parsedContent.forEach(line => {
+      let test = /-.*issues\/(\d*)/gi.exec(line);
+      let issueNumber = test[1];
+      github.issues.get({'owner':owner_name,'repo': repo_name,'number': issueNumber},(err,res)=>{
+        if(err){
+          console.log('there was an error'+err);
+        }
+        console.log(line.replace(/\[.*\](.*)/,'[ ] ['+res.data.title+']($1)'));
+//        console.log(res.data.title);
+      })   
   });
-//   console.log(prettyjson.render(res.data)); 
 });
